762fc6eabd147240d102d16867d90778d430361f
Wietse-Gijbels <wietse.gijbels@student.ucll.be>
2026-03-17 10:22:01 +0100

    start fruit

---
+++ Fruitshop/Fruitshop.Domain/Fruit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruitshop.Domain
+{
+    public class Fruit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Season { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Season})";
+        }
+    }
+}
